fe0ec04e04328bef40ccde44a959e1ddfd6d77f6
hc <2502611416@qq.com>
2020-02-10 14:08:50 +0800

    项目起步

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+import 'amfe-flexible/index.min.js'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
